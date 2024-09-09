29ae960091b51803aeae3b7b9da2c74c54a58e62
Radhikesh Kumar Thakur <116291105+RADHIKESHS@users.noreply.github.com>
2024-09-09 16:58:41 +0530

    both in same repo

---
+++ Theoretical_Challenge/src/main/java/com/prospecta/Theoretical_Challenge/TheoreticalChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.prospecta.Theoretical_Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheoreticalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheoreticalChallengeApplication.class, args);
+		System.out.println("Application running.....");
+	}
+
+}
